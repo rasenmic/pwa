f9a4025c4d8c83fc290b784c475cd62638bdd21f
Michael Rasenberger <michael.rasenberger@bdr.de>
2022-02-03 10:49:46 +0100

    drastic retry

--- sw.js
+++ sw.js
@@ -1,34 +1,25 @@
-var cacheName = 'video-pwa';
+var cacheName = 'hello-pwa';
 var filesToCache = [
-    './',
-    './login.html',
-    './index.html',
-    './style.css',
-    './main.js',
-    './Video.js',
-    './Info.js',
-    './Prescription.js',
-    './vaadin-router.js',
-    './favicon.ico',
-    './images/camera.png',
-    './images/info.png',
-    './images/document-signed.png',
+  './',
+  './index.html',
+  './css/style.css',
+  './js/main.js'
 ];
 
 /* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll(filesToCache);
-        })
-    );
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
